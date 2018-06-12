7cf73498275860b543d9d9bebf7233f2f9e9cf30
dunossauro <mendesxeduardo@gmail.com>
2018-06-12 01:09:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dia dos namorados
+
+Você já pensou em dizer que ama seu cônjuge usando ascii e cores??
+
+#### Modo de uso:
+![alt text](./giphy.gif)
---
+++ love.py
@@ -0,0 +1,40 @@
+from os import system
+from random import choice
+from time import sleep
+from click import command, option, echo
+from colorama import init, Back
+from pyfiglet import Figlet
+
+init(autoreset=True)
+
+back = list(vars(Back).values())
+
+
+def clear():
+    try:
+        system('clear')
+    except Exception:
+        system('cls')
+
+
+def full_style(love_string: str) -> str:
+    f = Figlet(font='doom')
+    selected_back = choice(back)
+    return f'{selected_back}{f.renderText(love_string)}'
+
+
+@command()
+@option('--name', prompt='Nome do seu amor')
+def love(name):
+    loved = 'Eu te amo ' + name
+    while True:
+        try:
+            echo(full_style(loved))
+            clear()
+            sleep(0.3)
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == '__main__':
+    love()
